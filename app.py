5e6b712da0fe9f0ad43ba465d4f0f36e829d08f0
Pim Snel <post@pimsnel.com>
2024-11-12 19:47:34 +0100

    initial working app

--- app.py
+++ app.py
@@ -4,12 +4,43 @@ from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 from dotenv import load_dotenv
+
+from zammadoo import Client
+
 load_dotenv()
 
-apptoken = os.environ.get("SLACK_BOT_TOKEN")
-bottoken = os.environ.get("SLACK_APP_TOKEN")
+APPTOKEN = os.environ.get("SLACK_BOT_TOKEN")
+BOTTOKEN = os.environ.get("SLACK_APP_TOKEN")
+ZAMMAD_URL = os.environ.get("ZAMMAD_URL")
+ZAMMAD_USER = os.environ.get("ZAMMAD_USER")
+ZAMMAD_PASSWD = os.environ.get("ZAMMAD_PASSWD")
 
-app = App(token=bottoken)
+app = App(token=BOTTOKEN)
+
+zamclient = Client(ZAMMAD_URL+"/api/v1/", http_auth=(ZAMMAD_USER, ZAMMAD_PASSWD))
+# or use an API token created via https://myhost.com/#profile/token_access
+#zamclient = Client("https://myhost.com/api/v1/", http_token="<token>")
+
+# I have a new ticket with id 17967 and need to download the attachment file
+#path = client.tickets(17967).articles[0].attachments[0].download()
+#print(f"The downloaded file is {path}")
+ticket = zamclient.tickets(28)
+print(f"ticket id: {ticket.id}")
+print(f"ticket number: #{ticket.number}")
+print(f"ticket title: {ticket.title!r}")
+
+# I need to append a new ticket article with attached files
+#client.ticket(17967).create_article("Server down again. See logfiles.", files=["kern.log", "syslog"])
+
+# I want to close all tickets with the tag "deprecated" and remove the tag
+#for ticket in client.tickets.search("tags:deprecated"):
+#    ticket.update(state="closed")
+#    ticket.remove_tags("deprecated")
+
+
+
+
+
 
 @app.shortcut("create_ticket")
 def create_ticket(ack, shortcut, client):
@@ -18,7 +49,8 @@ def create_ticket(ack, shortcut, client):
 
     ack()
     #pprint.pp(shortcut["message"]["text"])
-    message_text = shortcut["message"]["text"]
+    message = shortcut["message"]["text"]
+    message_text = message
 
     pprint.pp(shortcut["message"])
 
@@ -135,16 +167,27 @@ def create_ticket(ack, shortcut, client):
         }
     )
 
+
+
+def get_field_value_from_modal_body(modal_body, field_id):
+    for key, value in modal_body['view']['state']['values'].items():
+        for key2, value2 in value.items():
+            print(key2)
+            if key2 == field_id:
+                return value2['value']
+
 @app.view("")
 def handle_view_submission_events(ack, body, logger):
     ack()
-    pprint.pp(body)
-    pprint.pp(body['view']['state']['values'])
 
+    title = get_field_value_from_modal_body(body, "inputid_ticket_title" )
+    text = get_field_value_from_modal_body(body, "inputid_ticket_text" )
+
+    zamclient.tickets.create(title, "Managed Services Supportdesk", "", text)
 
 # Start your app
 if __name__ == "__main__":
-    SocketModeHandler(app, apptoken).start()
+    SocketModeHandler(app, APPTOKEN).start()
 
 
 
